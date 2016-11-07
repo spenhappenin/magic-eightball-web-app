9a8f78525d6bd99259aad22921167f798672e529
Spencer Richards <spencer.richards7@gmail.com>
2016-11-06 19:11:16 -0700

    app finished

--- server.rb
+++ server.rb
@@ -2,12 +2,7 @@ require 'pry'
 require 'sinatra'
 require 'babbler'
 
-get '/' do
-	erb :index
-end
-
-post '/' do 
-	@default_answers = [
+	$default_answers = [
 		'It is certain.',
 		'It is decidedly so.',
 		'Without a doubt.',
@@ -24,35 +19,51 @@ post '/' do
 		'Not likely',
 		'Bwahahaha! Ya... right.'
 		] 
-	@answer = @default_answers.sample		
+
+	$new_answers = $default_answers.clone
+
+get '/' do
+	erb :index
+end
+
+post '/' do 
+	@answer = $new_answers.sample		
 	@question = params[:question]
 	erb :index
 end
+
+get '/show' do 
+
+	erb :show
+end
+
 
 get '/create' do
 	erb :create
 end
 
 post '/create' do 
-	@default_answers = [
-		'It is certain.',
-		'It is decidedly so.',
-		'Without a doubt.',
-		'Yes, definitely.',
-		'You may rely on it.',
-		'Does a bear shit in the woods?',
-		'Most likely.',
-		'Outlook good.',
-		'No',
-		'Unfortunately no.',
-		'Try asking again later.',
-		'Not a single chance!',
-		'If I said yes, would that make you feel better?',
-		'Not likely',
-		'Bwahahaha! Ya... right.'
-		] 
-	@answer = @default_answers.sample		
-	@question = params[:question]
-	erb :index
+	@add_answer = params[:add_answer]
+	erb :create
 end
+
+get '/reset' do 
+	erb :reset
+end
+
+post '/reset' do 
+	@yes = params[:yes]
+	erb :reset
+end 
+
+
+
+
+
+
+
+
+
+
+
 
